63964271db43c7e6432631f38c0f937792c9abe5
Adrian Cotfas <adrcotfas@gmail.com>
2026-07-08 13:03:58 +0300

    chore: update source code header license text

--- .spotless/license.kt
+++ .spotless/license.kt
@@ -1,6 +1,6 @@
 /**
  *     Goodtime Productivity
- *     Copyright (C) 2025 Adrian Cotfas
+ *     Copyright (C) 2026 Adrian Cotfas
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
